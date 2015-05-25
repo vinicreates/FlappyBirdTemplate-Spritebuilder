1a3b27a4c83a2c28d713ad6ac1a3cbd0d90ea36c
Ashish Agnihotri <vinicreates@gmail.com>
2015-05-25 01:07:34 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,22 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    // Check if 2 seconds have passed
+    if(timeSinceObstacle > 2.0f){
+        // Add ew obstacle
+        [self addObstacle];
+        
+        // Reinitialize the timer variable
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
